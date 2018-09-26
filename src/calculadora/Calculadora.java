3e241ecfd3470e7e34a935338c92fcc8f27e64c4
AMX <AMX@AMX>
2018-09-26 16:15:37 +0200

    Hem afegit que demani dos números.

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,8 +1,17 @@
 package calculadora;
 
+import java.util.Scanner;
+
 public class Calculadora {
+    
+    Scanner in = new Scanner(System.in);
+ 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a, b;
         
+        System.out.println("Digues un número: "); a = in.nextInt();
+        System.out.println("Digues un altre número: "); b = in.nextInt();
     }
     
 }
